5d514677d7f05fd8e7b7f71f6c2b7027ca643140
Nikishma09 <nikishma.nakarmi5@gmail.com>
2024-07-31 17:53:32 +0545

    first commit

---
+++ src/First/Circle.java
@@ -0,0 +1,12 @@
+package First;
+import java.util.Scanner;
+public class Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println("The area of the circle is " + area);
+
+    }
+}
---
+++ src/First/sum.java
@@ -0,0 +1,10 @@
+package First;
+
+class sum {
+    public static void main(String[] args){
+        int a=4;
+        int b=9;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
